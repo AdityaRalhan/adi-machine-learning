6e17fb3ea5c2e5d8361260ed4d56277b640f5c37
Aditya Ralhan <152467920+AdityaRalhan@users.noreply.github.com>
2024-09-29 16:47:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Machine Learning app')
 
 st.write('this app builds an ML model!')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+df
